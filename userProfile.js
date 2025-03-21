11663660566ffb5b998b287d845a04eec491fcb7
rheaaa16 <rfabroborbe@gmail.com>
2025-03-21 22:14:21 +0800

    Create userProfile.js

---
+++ userProfile.js
@@ -0,0 +1,9 @@
+let Full_Name = prompt("Please enter your full name");
+let Age = prompt("Please enter your age");
+let Fav_Num = prompt("Please enter your favnumber");
+let Fav_Color = prompt("Enter your favorite color");
+
+console.log("Full name:", Full_Name);
+console.log("Age:", Age);
+console.log("Favorite Number:", Fav_Num);
+console.log("Favorite Color:", Fav_Color);
