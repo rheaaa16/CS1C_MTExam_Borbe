a4fe44a9f2655c9425ea0dd175175ae5f4851340
rheaaa16 <rfabroborbe@gmail.com>
2025-03-21 22:19:30 +0800

    Create colorPreference.js

---
+++ colorPreference.js
@@ -0,0 +1,7 @@
+let colors= [];
+
+for(let i = 0; i < 3; i++){
+    let FavColors = prompt("Please enter your favorite color");
+    colors.push(FavColors);
+    console.log("Updated list of colors:", colors);
+}
